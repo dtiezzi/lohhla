300e41c5a6a0cbf7cc161bf082ae410815594b37
dtiezzi <dtiezzi@usp.br>
2021-07-21 17:53:58 -0300

    fixing 1

--- LOHHLAscript_original1.R
+++ LOHHLAscript_original1.R
@@ -673,7 +673,8 @@ if(mapping.step){
 
   for(BAMfile in BAMfiles){
     
-    BAMid <- unlist(strsplit(BAMfile, split = '.bam'))[1]
+    #BAMid <- unlist(strsplit(BAMfile, split = '.bam'))[1]
+    BAMid <- stringr::str_sub(dplyr::last(unlist(strsplit(l[1], split = '/'))),1, -5)
     
     ## I have no idea why this is used here...
 
